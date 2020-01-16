1583159786134c63fb07636fafe73a253384dc1a
Mandy Leung <mleung32@gmail.com>
2020-01-15 16:05:05 -0800

    Updates

---
+++ precache-manifest.fbe0e1379c9b1a4b360b3552df209bb2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "695f7e85283dad6ff87258b6a5a371f1",
+    "url": "/info-tracks/index.html"
+  },
+  {
+    "revision": "cd4eb53a615d5800c85e",
+    "url": "/info-tracks/static/css/main.cda12489.chunk.css"
+  },
+  {
+    "revision": "a19017d468e850aa5a39",
+    "url": "/info-tracks/static/js/2.0c9a705a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/info-tracks/static/js/2.0c9a705a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cd4eb53a615d5800c85e",
+    "url": "/info-tracks/static/js/main.2cd5d5bf.chunk.js"
+  },
+  {
+    "revision": "b4d73543c49696c182e3",
+    "url": "/info-tracks/static/js/runtime-main.3b277d7b.js"
+  },
+  {
+    "revision": "cefa185fdd6910ac5537f9206a4f86f8",
+    "url": "/info-tracks/static/media/dsImage.cefa185f.jpg"
+  },
+  {
+    "revision": "53252df24b0dbf31b6cf4d369c32a492",
+    "url": "/info-tracks/static/media/pmImage.53252df2.jpg"
+  },
+  {
+    "revision": "58fd4aebb3d37aeb85229e142625ff02",
+    "url": "/info-tracks/static/media/sweImage.58fd4aeb.png"
+  },
+  {
+    "revision": "ce0036c1ec58a50332ec0002a5498c44",
+    "url": "/info-tracks/static/media/uxImage.ce0036c1.jpg"
+  }
+]);
